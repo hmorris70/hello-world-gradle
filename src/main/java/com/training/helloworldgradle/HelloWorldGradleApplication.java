c247bfa06bae8cd9032802b4ce1bc38ca14f061a
Harry Morris <harry@gaw-morris.com>
2020-05-21 15:12:27 +0800

    initial commit

---
+++ src/main/java/com/training/helloworldgradle/HelloWorldGradleApplication.java
@@ -0,0 +1,13 @@
+package com.training.helloworldgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldGradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/training/helloworldgradle/HelloWorldGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.helloworldgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
